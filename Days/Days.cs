3bb554646b1cf8a071377c1b49ee682237a0455b
Lee Beenen <leebeenen@gmail.com>
2021-12-01 14:19:35 +0100

    Day1 pt1

--- Days/Days.cs
+++ Days/Days.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Linq;
 
 public static partial class Days
 {
@@ -11,6 +13,32 @@ public static partial class Days
 
   public static string Day1()
   {
-    return OutputResult("day1", "day2");
+//     var testInput = @"199
+// 200
+// 208
+// 210
+// 200
+// 207
+// 240
+// 269
+// 260
+// 263";
+
+    var input = File.ReadAllLines(Path.Combine(InputBasePath, "Day1.txt")).Select(int.Parse);
+
+    var start = 0;
+    var current = 0;
+    var increases = 0;
+
+    foreach (var line in input)
+    {
+      current = line;
+
+      if (current > start && start > 0) increases++;
+
+      start = current;
+    }
+
+    return OutputResult(increases.ToString(), "day2");
   }
 }
