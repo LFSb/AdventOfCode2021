d399df1ff6146371e72a2d24cf40366f601d0335
Lee Beenen <leebeenen@gmail.com>
2021-12-01 14:33:59 +0100

    Day1 Pt2

--- Days/Days.cs
+++ Days/Days.cs
@@ -13,32 +13,43 @@ public static partial class Days
 
   public static string Day1()
   {
-//     var testInput = @"199
-// 200
-// 208
-// 210
-// 200
-// 207
-// 240
-// 269
-// 260
-// 263";
+    var testInput = @"199
+200
+208
+210
+200
+207
+240
+269
+260
+263";
 
-    var input = File.ReadAllLines(Path.Combine(InputBasePath, "Day1.txt")).Select(int.Parse);
+    var input = //testInput.Split(Environment.NewLine).Select(x => int.Parse(x)).ToArray(); //
+    File.ReadAllLines(Path.Combine(InputBasePath, "Day1.txt")).Select(x => int.Parse(x)).ToArray();
 
     var start = 0;
     var current = 0;
-    var increases = 0;
+    var p1 = 0;
+    var p2 = 0;
 
-    foreach (var line in input)
+    for (var idx = 0; idx < input.Length; idx++)
     {
-      current = line;
+      current = input[idx];
 
-      if (current > start && start > 0) increases++;
+      if (current > start && start > 0) p1++;
+
+      if (idx < input.Length - 3)
+      {
+        //we still have enough to calculate B.
+        var a = input[idx] + input[idx + 1] + input[idx + 2];
+        var b = input[idx + 1] + input[idx + 2] + input[idx + 3];
+
+        if (b > a) p2++;
+      }
 
       start = current;
     }
 
-    return OutputResult(increases.ToString(), "day2");
+    return OutputResult(p1.ToString(), p2.ToString());
   }
 }
